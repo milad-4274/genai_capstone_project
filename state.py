35d2742fab27d6294cf1041f9ab86d6585cf870e
milad <milad.4274@gmail.com>
2025-04-18 01:25:55 +0330

    update state

--- state.py
+++ state.py
@@ -7,33 +7,39 @@ class TripState:
     chat_history: List[str] = None
     user_preferences: str = None
     current_location: str = None
-    destination: str = None
-    accommodation: str = None
-    visa_eligibility: str = None
     budget: str = None
-    destination_info: str = None
+    destination: str = None
+    visa_info: str = None
     itinerary_draft: str = None
     personalized_itinerary: str = None
-    messages: List[BaseMessage]
+    accommodation: str = None
+    duration : str = None
+    start_date : str = None
+    trip_tips : str = None
+    destination_activity: str = None
+    transportation : str = None
     next_node: str = None # Added for supervisor
     agent_input: str = None # Added for supervisor
-    response: str = None # Added for final response
-
+    
     def __init__(self, chat_history: str, **kwargs):
         self.chat_history = [chat_history]
         self.user_preferences = kwargs.get("user_preferences", chat_history)
         self.current_location = kwargs.get("current_location")
-        self.destination = kwargs.get("destination")
-        self.accommodation = kwargs.get('accommodation')
-        self.visa_eligibility = kwargs.get("visa_eligibility")
         self.budget = kwargs.get("budget")
-        self.destination_info = kwargs.get("destination_info")
+        self.destination = kwargs.get("destination")
+        self.visa_info = kwargs.get("visa_info")
         self.itinerary_draft = kwargs.get("itinerary_draft")
         self.personalized_itinerary = kwargs.get("personalized_itinerary")
-        self.next_node = kwargs.get("next_node") # Added
-        self.agent_input = kwargs.get("agent_input") # Added
-        self.response = kwargs.get("response")
-    
+        self.accommodation = kwargs.get("accommodation")
+        self.duration = kwargs.get("duration")
+        self.start_date = kwargs.get("start_date")
+        self.trip_tips = kwargs.get("trip_tips")
+        self.destination_activity = kwargs.get("destination_activity")
+        self.transportation = kwargs.get("transportation")
+        self.next_node = kwargs.get("next_node")
+        self.agent_input = kwargs.get("agent_input")
+        # self.response = kwargs.get("response").
+        
     def get_chat_history(self):
         # print("Appending to Chat history", response)
         # self.chat_history.append(response)
@@ -44,16 +50,19 @@ class TripState:
     chat_history : {self.chat_history}
     user_preferences : {self.user_preferences}
     current_location : {self.current_location}
-    destination : {self.destination}
-    accommodation: {self.accommodation}
-    visa_eligibility : {self.visa_eligibility}
     budget : {self.budget}
-    destination_info : {self.destination_info}
+    destination : {self.destination}
+    visa_info : {self.visa_info}
     itinerary_draft : {self.itinerary_draft}
     personalized_itinerary : {self.personalized_itinerary}
+    accommodation : {self.accommodation}
+    duration : {self.duration}
+    start_date : {self.start_date}
+    trip_tips : {self.trip_tips}
+    destination_activity : {self.destination_activity}
+    transportation : {self.transportation}
     next_node : {self.next_node}
     agent_input : {self.agent_input}
-    response : {self.response}
     """
     
     def __str__(self):
@@ -61,14 +70,17 @@ class TripState:
     chat_history : {self.chat_history}
     user_preferences : {self.user_preferences}
     current_location : {self.current_location}
-    destination : {self.destination}
-    accommodation: {self.accommodation}
-    visa_eligibility : {self.visa_eligibility}
     budget : {self.budget}
-    destination_info : {self.destination_info}
+    destination : {self.destination}
+    visa_info : {self.visa_info}
     itinerary_draft : {self.itinerary_draft}
     personalized_itinerary : {self.personalized_itinerary}
+    accommodation : {self.accommodation}
+    duration : {self.duration}
+    start_date : {self.start_date}
+    trip_tips : {self.trip_tips}
+    destination_activity : {self.destination_activity}
+    transportation : {self.transportation}
     next_node : {self.next_node}
     agent_input : {self.agent_input}
-    response : {self.response}
     """
